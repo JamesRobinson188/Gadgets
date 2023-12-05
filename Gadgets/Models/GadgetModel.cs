f847a48b4871389241af51d900a9be3a742986ea
jamesrobinson <jamesrobinson@VIC11>
2023-12-05 11:30:24 +0000

    Add project files.

---
+++ Gadgets/Models/GadgetModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gadgets.Models
+{
+    public class GadgetModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string AppearsIn { get; set; }
+
+        public string WithThisActor { get; set; }
+
+        public GadgetModel()
+        {
+            Id = -1;
+            Name
+        }
+    }
+}
---
+++ Gadgets/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Gadgets.Startup))]
+namespace Gadgets
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
